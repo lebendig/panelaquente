26c7f77ea9acd94e47b8d2fe4ab8d12ec2a86379
unknown <lebendig@hotmail.com>
2022-10-01 17:33:32 +0200

    upload project

---
+++ panelaquente/src/main/java/br/com/panelaquente/PanelaquenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.panelaquente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanelaquenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanelaquenteApplication.class, args);
+	}
+
+}
---
+++ panelaquente/src/test/java/br/com/panelaquente/PanelaquenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.panelaquente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanelaquenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
